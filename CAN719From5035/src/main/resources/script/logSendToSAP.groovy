6e1232e457a694fc2d7e2f570f86375dc6885a34
Justin <apvancouver@icloud.com>
2018-06-26 00:09:42 -0700

    pmap change

--- CAN719From5035/src/main/resources/script/logSendToSAP.groovy
+++ CAN719From5035/src/main/resources/script/logSendToSAP.groovy
@@ -9,7 +9,6 @@ def Message processData(Message message) {
 
 	def pmap = message.getProperties();
 	String enableLogging = pmap.get("ENABLE_LOGGING");
-    enableLogging = "TRUE";
 	if(enableLogging != null && enableLogging.toUpperCase().equals("TRUE")){
 		def body = message.getBody(java.lang.String) as String;
 		def messageLog = messageLogFactory.getMessageLog(message);
